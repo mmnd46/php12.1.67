037d1cd1eaad5e895f2fe4154ca31642a94626f3
mmnd46 <154506757+mmnd46@users.noreply.github.com>
2024-01-19 13:14:18 +0700

    Create cookie-login.php

---
+++ cookie-login.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <form action="cookie.php" method="POST">
+            username: <input type="text" name="username"><br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
